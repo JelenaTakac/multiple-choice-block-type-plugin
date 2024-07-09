7783f2b41045fcc2250deaf6677d51b71dc8d3c1
JelenaTakac <jelena.andjelic.gr@gmail.com>
2024-07-09 15:01:08 +0200

    Event handling and updating Block attributes

--- src/index.js
+++ src/index.js
@@ -1,30 +1,65 @@
+import "./index.scss";
+import { useState } from "react";
+import { TextControl, Flex, FlexBlock, FlexItem, Button, Icon } from "@wordpress/components";
+
 wp.blocks.registerBlockType("ourplugin/are-you-paying-attention", {
   title: "Are You Paying Attention?",
   icon: "smiley",
   category: "common",
   attributes: {
-    skyColor: {type: "string"},
-    grassColor: {type: "string"}
+    question: {type: "string"},
+    answers: {type: "array", default: ["red", "blue", "orange"]}
   },
-  edit: function (props) {
-
-    function updateSkyColor(e) {
-        props.setAttributes({skyColor: e.target.value});
-    }
-    function updateGrassColor(e) {
-        props.setAttributes({grassColor: e.target.value});
-    }
-    // return wp.element.createElement("h3", null, "This is on edit post/page.");
-    return (
-        <div>
-            <input type="text" placeholder="sky color" value={props.attributes.skyColor} onChange={updateSkyColor}/>
-            <input type="text" placeholder="grass color" value={props.attributes.grassColor} onChange={updateGrassColor}/>
-        </div>
-    );
-  },
+  edit: EditComponent,
   save: function (props) {
     return null;
   }
-})
+});
 
 
+function EditComponent(props) {
+
+    function updateQuestion(value) {
+        props.setAttributes({question: value});
+    }
+
+    function updateAnswer(value, index) {
+        const newAnswers = [...props.attributes.answers];
+        newAnswers[index] = value;
+        props.setAttributes({ answers: newAnswers });
+    }
+
+    function addNewAnswer() {
+        props.setAttributes({ answers: [...props.attributes.answers, ""] });
+    }
+
+    function deleteAnswer(indexToDelete) {
+        const newAnswers = props.attributes.answers.filter((answer, i) => answer[i] != answer[indexToDelete]);
+        props.setAttributes({ answers: newAnswers });
+    }
+    // return wp.element.createElement("h3", null, "This is on edit post/page.");
+    return (
+        <div className="paying-attention-edit-block">
+            <TextControl label="Question:" style={{fontSize: "20px"}} value={props.attributes.question} onChange={updateQuestion}/>
+            <p style={{fontSize: "13px", margin: "20px 0 8px 0"}}>Answers:</p>
+            {props.attributes.answers.map((answer, index) => {
+                return (
+                    <Flex key={index}>
+                        <FlexBlock>
+                            <TextControl value={answer} onChange={newValue => updateAnswer(newValue, index)}/>
+                        </FlexBlock>
+                        <FlexItem>
+                            <Button>
+                                <Icon className="mark-as-correct" icon="star-empty"/>
+                            </Button>
+                        </FlexItem>
+                        <FlexItem>
+                            <Button variant="link" className="attention-delete" onClick={() => deleteAnswer(index)}>Delete</Button>
+                        </FlexItem>
+                    </Flex>
+                )
+            })}
+            <Button variant="primary" onClick={addNewAnswer}>Add another answer</Button>
+        </div>
+    );
+  }
