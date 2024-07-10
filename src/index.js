88739d1cc596885b17879915cbce8060125c6665
JelenaTakac <jelena.andjelic.gr@gmail.com>
2024-07-10 08:37:41 +0200

    Imported React for displaying block type content on frontend

--- src/index.js
+++ src/index.js
@@ -1,26 +1,6 @@
 import "./index.scss";
 import { TextControl, Flex, FlexBlock, FlexItem, Button, Icon } from "@wordpress/components";
 
-// (function() {
-//     let locked = false;
-
-//     wp.data.subscribe(function() {
-//         const results = wp.data.select("core/block-editor").getBlocks().filter(function(block) {
-//       return block.name == "ourplugin/are-you-paying-attention" && block.attributes.correctAnswer == undefined
-//     })
-//     });
-
-//     if (results.length && locked == false) {
-//         locked = true;
-//         wp.data.dispatch("core/editor").lockPostSaving("noanswer");
-//     }
-
-//     if (!results.length && locked) {
-//         locked = false;
-//         wp.data.dispatch("core/editor").unlockPostSaving("noanswer");
-//     }
-// })();
-
 (function() {
   let locked = false
   
